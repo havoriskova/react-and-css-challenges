3beec6c53ec65c7c334b20beae8d57de5f552f10
havoriskova <ha.voriskova@gmail.com>
2022-08-01 12:43:50 +0200

    updated DotWave component
    - created dotted rows with map function (with array "circle")
    - handy radial gradient for quick determination of the center of the container
    - animation delay for each row

--- src/challenges/DotWave.js
+++ src/challenges/DotWave.js
@@ -3,21 +3,64 @@ import "./DotWave.css";
 
 export default function DotWave() {
 
-    let circles = [10, 20, 30, 40, 50]; // po určitým počtu lines změníš výšku a počet  lines v nadřazený smyčce ?
-    let dots = [8, 12, 16, 20, 30]; // class "line", měníš transform rotate
+   {/* 360: 12 TEČEK = 30 STUPŇŮ
+       360:  15 teček = 24 st - málo
+       360: 18 = 20st
+       360: 20 TEČEK = 18 STUPŇŮ - málo
+       360: 24 teček = 15 st
+       360: 30 teček = 12 stupňů
+       360: 40 TEČEK = 9 STUPŇŮ
+       360: 50 teček = 7.2 stupňů
+       360: 60 TEČEK = 6 STUPŇŮ
+   */}
+
+    let dotWaveCircles = [
+    [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120], 
+    [20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20], 
+    [15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15],
+    [12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12],
+    [9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9],
+    [7.2, 7.2, 7.2, 7.2, 7.2, 7.2, 7.2, 7.2, 7.2, 7.2, 7.2, 7.2, 7.2, 7.2, 7.2, 7.2, 7.2, 7.2, 7.2, 7.2, 7.2, 7.2, 7.2, 7.2, 7.2, 7.2, 7.2, 7.2, 7.2, 7.2, 7.2, 7.2, 7.2, 7.2, 7.2, 7.2, 7.2, 7.2, 7.2, 7.2, 7.2, 7.2, 7.2, 7.2, 7.2, 7.2, 7.2, 7.2, 7.2, 7.2],
+    [6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6]
+    ]; // po určitým počtu lines změníš výšku a počet  lines v nadřazený smyčce ?
+
 
     return (
         <div className="DotWave frame">
-          <div className="circle">
+          {/* <div className="circle">
              <div className="line">
                 <div className="dot"></div>
              </div>
 
-          </div>
+          </div> */}
            
 
-          {circles.map((value, index) => {return <div className="circles" key={index} data-key={value} style={{transform:`rotate(${value*3}deg)`}}>
-                  {dots.map((value, index) => {return <span key={index} data-key={value} className="line"><span className="dot"></span></span>})}
+          {dotWaveCircles[0].map((value, index) => {return <div className="circles" key={index} data-key={value} style={{transform:`rotate(${value*3}deg)`, height: "0rem"}}>
+                 <span className="line"><span className="dot" style={{animationDelay:"0"}}></span></span>
+          </div>})}
+
+         {dotWaveCircles[1].map((value, index) => {return <div className="circles" key={index} data-key={value} style={{transform:`rotate(${value*index}deg)`, height:"2rem"}}>
+                <span className="line"><span className="dot" style={{animationDelay:".5s"}}></span></span>
+          </div>})}
+
+         {dotWaveCircles[2].map((value, index) => {return <div className="circles" key={index} data-key={value} style={{transform:`rotate(${value*index}deg)`, height:"4rem"}}>
+                <span className="line"><span className="dot" style={{animationDelay:"1s"}}></span></span>
+          </div>})}
+
+          {dotWaveCircles[3].map((value, index) => {return <div className="circles" key={index} data-key={value} style={{transform:`rotate(${value*index}deg)`, height:"6rem"}}>
+                <span className="line"><span className="dot" style={{animationDelay:"1.5s"}}></span></span>
+          </div>})}
+
+            {dotWaveCircles[4].map((value, index) => {return <div className="circles" key={index} data-key={value} style={{transform:`rotate(${value*index}deg)`, height:"8rem"}}>
+                <span className="line"><span className="dot" style={{animationDelay:"2s"}}></span></span>
+          </div>})}
+
+            {dotWaveCircles[5].map((value, index) => {return <div className="circles" key={index} data-key={value} style={{transform:`rotate(${value*index}deg)`, height:"10rem"}}>
+                <span className="line"><span className="dot" style={{animationDelay:"2.5s"}}></span></span>
+          </div>})}
+
+           {dotWaveCircles[6].map((value, index) => {return <div className="circles" key={index} data-key={value} style={{transform:`rotate(${value*index}deg)`, height:"12rem"}}>
+                <span className="line"><span className="dot" style={{animationDelay:"3s"}}></span></span>
           </div>})}
            {/* map Circle, a v něm map Dots vč. počtu Dots skrz i ?? 
            
